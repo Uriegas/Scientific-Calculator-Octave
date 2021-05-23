fd74793b0a2013fc64bf2e57a97cb3929bd22394
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-05-22 19:04:58 -0500

    TknMeaning. Implementation from: https://youtu.be/OJt6C5efEYM

---
+++ src/main/java/com/spolancom/TknMeaning.java
@@ -0,0 +1,34 @@
+package com.spolancom;
+/**
+ * Represents the meaning of the tokens
+ * it is used for generating the tokens
+ * in the Tokenizer
+ */
+public class TknMeaning {
+    private static int tokenType;//For meanning
+    private String regex;//For searching
+    
+    /**
+     * Simple Constructor
+     * @param t tokenType
+     * @param r regex expression
+     */
+    public TknMeaning(int t, String r){
+        tokenType = t;
+        regex = "^(" + r + ")";
+    }
+    /**
+     * Get the token type
+     * @return token type aka. meaning
+     */
+    public int getTokenType(){
+        return tokenType;
+    }
+    /**
+     * Get the regex syntax for searching
+     * @return regex ex. \\d+ (number)
+     */
+    public String getRegex(){
+        return regex;
+    }
+}
