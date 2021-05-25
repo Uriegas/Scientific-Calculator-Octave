33cf8dd8d11fe850cd69ba9a134e47de9df81dc4
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-05-25 18:22:43 -0500

    Works with functions and variables

--- src/test/java/com/spolancom/ParserTest.java
+++ src/test/java/com/spolancom/ParserTest.java
@@ -31,7 +31,20 @@ public class ParserTest {
     }
     @Test
     public void testFunctionCall(){
+        Parser p = new Parser();
+        Interpreter evalVisitor = new Interpreter();
 
+       // Exp tree = p.parse("x = 10+10");
+       // Object result = tree.accept(evalVisitor);
+        Exp tree = p.parse("print(x)");
+        Object result = tree.accept(evalVisitor);
+        PrintTree visit = new PrintTree();
+        System.out.println("Expression is: " + tree.accept(visit) + "\nReturn value is: " + String.valueOf(result));
+        tree = p.parse("sin(10+cos(x+2)-70)");
+        result = tree.accept(evalVisitor);
+        
+        System.out.println("Expression is: " + tree.accept(visit) + "\nReturn value is: " + String.valueOf(result));
+        assertEquals(Math.sin((10.0+Math.cos(4+2))-70), Double.valueOf((Double)result), 0.01);
     }
     @Test
     public void TestSAVE(){
