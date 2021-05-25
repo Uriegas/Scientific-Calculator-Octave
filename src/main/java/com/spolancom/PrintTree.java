3809fd39e4380866bd4d45393bdfe21c8bebc1e6
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-05-25 08:48:02 -0500

    Define a printing interface for the nodes

---
+++ src/main/java/com/spolancom/PrintTree.java
@@ -0,0 +1,55 @@
+package com.spolancom;
+
+public class PrintTree implements Exp.Visitor<String> {
+    String print(Exp expr) {
+        return expr.accept(this);
+    }
+    /**
+     * Add the value and expression to the environment
+     * map<value, expression>
+     */
+    @Override
+    public String visitAssignExpr(Exp.AssignNode expr){
+        return expr.name.getValue() + " = " + expr.value.accept(this);
+    }
+    /**
+     * Search for the function in the environment
+     * and execute it expression
+     */
+    @Override
+    public String visitCallExpr(Exp.CallNode expr){
+        String s = "";
+        s += expr.callee.accept(this) + "(";
+        for(Exp arg : expr.arguments)
+            s += arg.accept(this) + ", ";
+        s = s.substring(0, s.length()-1);
+        s = s.substring(0, s.length()-1);
+        return s += " )";
+    }
+    /**
+     * Evaluate inner expression an return result
+     */
+    @Override
+    public String visitGroupingExpr(Exp.GroupingNode expr){
+        return "( " + expr.expression.accept(this) + " )";
+    }
+    /**
+     * Just get the number inside this node
+     */
+    @Override
+    public String visitNumberExpr(Exp.NumberNode e){
+        return (String)e.value;
+    }
+    @Override
+    public String visitVariableExpr(Exp.Variable expr){
+        return expr.name.getValue();//Get the name of the token
+        //return lookupVariable(expr.name, expr);
+    }
+    /**
+     * This evalutes most of the mathematical functions
+     */
+    @Override
+    public String visitBinaryExpr(Exp.BinaryNode expr){
+        return "( " + expr.left.accept(this) + expr.operator.getValue() + expr.right.accept(this) + " )";
+    }
+}
